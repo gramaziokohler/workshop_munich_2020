97067020e9d908342e78ae2e5412dbda9dca46ee
Romana Rust <rust@arch.ethz.ch>
2020-02-22 11:16:30 +0100

    update frame examples

--- examples/003_frame_in_frame.py
+++ examples/003_frame_in_frame.py
@@ -1,24 +1,23 @@
 """Example: 'frame in frame'
 """
-from compas.geometry import * 
+from compas.geometry import Point
+from compas.geometry import Vector
+from compas.geometry import Frame
 
-# coordinate system F0
 point = Point(146.00, 150.00, 161.50)
 xaxis = Vector(0.9767, 0.0010, -0.214)
 yaxis = Vector(0.1002, 0.8818, 0.4609)
-F0 = Frame(point, xaxis, yaxis)
+F0 = Frame(point, xaxis, yaxis) # coordinate system F0
 
-# frame F in F0 (local coordinates)
 point = Point(35., 35., 35.)
 xaxis = Vector(0.604, 0.430, 0.671)
 yaxis = Vector(-0.631, 0.772, 0.074)
-F = Frame(point, xaxis, yaxis)
+f_lcf = Frame(point, xaxis, yaxis) # frame f_lcf in F0 (local coordinates)
 
 # frame in global (world) coordinate system
-F_wcf = F0.to_world_coords(F)
-print(F_wcf)
+f_wcf = F0.to_world_coords(f_lcf)
+print(f_wcf)
 
-# check
-F2 = F0.to_local_coords(F_wcf)
-print(F2)
-print(F == F2)
+f_lcf2 = F0.to_local_coords(f_wcf) # world coords back to local coords
+print(f_lcf2) # should equal f_lcf
+print(f_lcf == f_lcf2)
