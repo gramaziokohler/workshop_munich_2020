547e0a475f0b5e959a55dee21c781087f94e3e9d
Romana Rust <rust@arch.ethz.ch>
2020-11-05 15:33:49 +0100

    updates assembly

--- examples/045_pick_and_place.py
+++ examples/045_pick_and_place.py
@@ -11,12 +11,9 @@ from compas_fab.robots import Configuration
 from compas_fab.robots import Tool
 from compas_fab.robots import AttachedCollisionMesh
 from compas_fab.robots import CollisionMesh
-#from assembly import Element
-#from assembly import Assembly
 
-import sys
-sys.path.append(r"C:\Users\rustr\workspace\libraries\assembly_information_model\src")
-from assembly_information_model.assembly import Element, Assembly
+from assembly_information_model.assembly import Element
+from assembly_information_model.assembly import Assembly
 
 HERE = os.path.dirname(__file__)
 DATA = os.path.abspath(os.path.join(HERE, "..", "data"))
