ce3e4554a06dfd2b8d58eb12719100a4d6b9b684
Gonzalo Casas <casas@arch.ethz.ch>
2020-11-20 06:38:00 +0100

    Add joint state subscription script example

---
+++ examples/052_subscribe_joint_states.py
@@ -0,0 +1,29 @@
+import argparse
+
+import time
+
+from roslibpy import Topic
+from compas_fab.backends import RosClient
+
+def receive_message(message):
+    print(message)
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description='Joint states publisher')
+    parser.add_argument('-r', '--ros-host', help='ROS host', default='localhost')
+    parser.add_argument('-p', '--ros-port', help='ROS port', default=9090)
+    args = parser.parse_args()
+
+
+    client = RosClient(host=args.ros_host, port=args.ros_port)
+
+    # Subscribe to joint state updates
+    listener = Topic(client, '/ur_joint_states', 'sensor_msgs/JointState')
+    listener.subscribe(receive_message)
+
+    # Start connection
+    client.on_ready(lambda: print('[OK] Connected to ROS: \n'))
+    client.run_forever()
+
+    client.terminate()
