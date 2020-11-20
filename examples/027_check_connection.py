01361a4a95bd456b26311af340318d6978fc545c
Gonzalo Casas <casas@arch.ethz.ch>
2020-11-20 05:55:18 +0100

    Revert ngrok host in some examples

--- examples/027_check_connection.py
+++ examples/027_check_connection.py
@@ -1,6 +1,4 @@
 from compas_fab.backends import RosClient
 
-#with RosClient('localhost') as client:
-with RosClient('af41953ec075.ngrok.io', 80) as client:
-
+with RosClient('localhost') as client:
     print('Connected:', client.is_connected)
